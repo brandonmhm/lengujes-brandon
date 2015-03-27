404a747164baf9baa099a54b6455a9ea64c57d83
T-301 <T-301@PC332>
2015-03-26 19:43:18 -0600

    @daton

---
+++ src/Serializacion/PersistenciaUsuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Serializacion;
+
+/**
+ *
+ * @author T-301
+ */
+public class PersistenciaUsuario {
+    //void no tiene tipo de retosno
+    public static void guardar(Cliente c)throws Exception{
+        //aqui va logica muy loca pero a la ves hermosa
+        
+    }
+    
+    public static Cliente leer(String nombre)throws Exception{
+    //aqui tambien va codigo muy loco pero a la ves muy hermoso
+        
+    }
+    
+}
